4d939020f20e2267e09cb07f9998a78d02a2692b
peeweeScott <peeda@comcen.com.au>
2016-04-29 18:01:05 +1000

    Initial Commit

---
+++ Heat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heat
+//
+//  Created by Peter Scott on 29/04/2016.
+//  Copyright © 2016 Peter Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
